34ed18c409c563b7da7f2dfc711580a22903c789
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-22 14:47:53 +0100

    started navigating

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        // onPress={() => navigation.navigate('ReviewDetails')}
+        // With push even when I'm on the page that I'm pushing, the page gets added to the stack  
+        onPress={() => navigation.push('ReviewDetails')}
+      />
     </View>
   );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>Get rid of all the pages but the first one</Text>
+      <Button title="Pop to Top bb!" onPress={() => navigation.popToTop()} />
     </View>
   );
 }
