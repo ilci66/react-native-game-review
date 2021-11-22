d45f845fe3ef6fa9ade15e4deeb423fe5880719f
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-22 15:40:09 +0100

    sending data to screens

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
+
+
 export default function Home({ navigation }) {
+
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
+  
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button
+      {/* <Text style={globalStyles.titleText}>Home Screen</Text> */}
+      {/* <Button
         title="Go to Details"
         // onPress={() => navigation.navigate('ReviewDetails')}
         // With push even when I'm on the page that I'm pushing, the page gets added to the stack  
         onPress={() => navigation.push('ReviewDetails')}
-      />
+      /> */}
+      <FlatList data={reviews} renderItem={({ item }) => (
+        // Second parameter sends the data
+        // Well even though in the toruial it was carriend in the navition,
+        // In this example it threw an error so, look for the data in route as I did here in the future
+        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+          <Text style={globalStyles.titleText}>{ item.title }</Text>
+        </TouchableOpacity>
+      )} />
     </View>
   );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({ navigation }) {
+export default function ReviewDetails({ navigation, route }) {
+
+  console.log("params ==>",route.params)
+
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
+      {/* <Text>ReviewDetails Screen</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Text>Get rid of all the pages but the first one</Text>
-      <Button title="Pop to Top bb!" onPress={() => navigation.popToTop()} />
+      <Button title="Pop to Top bb!" onPress={() => navigation.popToTop()} /> */}
+      {/* <Text>Some details about the game</Text> */}
+      <Text>{ route.params.title }</Text>
+      <Text>{ route.params.body }</Text>
+      <Text>{ route.params.rating }</Text>
+
     </View>
   );
 }
